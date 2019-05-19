5c9af74d11baabbf916ace7c48b1884dc1f39906
Cody Jones <codymjones@ymail.com>
2019-05-19 16:21:11 -0700

    Tests mostly written, but doesn't actually fail when it should, somethign to do with promises :P

--- test/databaseInitTests.js
+++ test/databaseInitTests.js
@@ -1,87 +1,154 @@
-const initCards = require('../src/controllers/Mongo/init').initCards;
+const init = require('../src/controllers/Mongo/init');
 const CardModel = require('../src/models/CardModel/index');
 const CardRankModel = require('../src/models/CardRankModel/index');
 const SuitModel = require('../src/models/SuitModel/index');
+const PoliticalRankModel = require('../src/models/PoliticalRankModel/index');
 
 describe('Initiallizing Database from scratch.', () => {
 
-    before((done) => {
-        CardModel.deleteMany({}).then(() => {
-            CardRankModel.deleteMany({}).then(() => {
-                SuitModel.deleteMany({}).then(() => {done()});
-            });
-        });
-    });
-
-    describe('Check that db collections are empty.', () => {
-
-        it('No Card Ranks in DB', done => {
-            CardRankModel.countDocuments({}).then(count => {
-                if (count)
-                    done(new Error('There are ' + count + ' Card Ranks in the Database'));
-                else
-                    done();
-            });
-        });
-
-        it('No Suits in DB', done => {
-            SuitModel.countDocuments({}).then(count => {
-                if (count)
-                    done(new Error('There are ' + count + ' Suits in the Database'));
-                else
-                    done();
-            });
-        });
-
-        it('No Cards in DB', done => {
-            CardModel.countDocuments({}).then(count => {
-                if (count)
-                    done(new Error('There are ' + count + ' Cards in the Database'));
-                else
-                    done();
-            });
-        });
-
-    });
-
-    describe('Initializing Cards', () => {
-
-        it('initializeCards()', done => {
-            let error;
-
-            initCards().then(() => {
-                CardModel.countDocuments({}, numberOfCards => {
-                    if (numberOfCards != 52)
-                        error = new Error('There should be 52 cards, but there are ' + numberOfCards);
+    describe('InitCards()', () => {    
+        
+        before((done) => {
+            CardModel.deleteMany({}).then(() => {
+                CardRankModel.deleteMany({}).then(() => {
+                    SuitModel.deleteMany({}).then(() => {done()});
                 });
-            })
-            .catch(done)
-            .finally(() => {
-                if (error)
-                    done(error);
-                else
-                    done();
             });
         });
 
-        it('Check that calling initalize twice doesn\'t create duplicate instances', done => {
-            let error;
+        describe('Check that db collections are empty.', () => {
 
-            initCards().then(() => {
-                CardModel.countDocuments({}, numberOfCards => {
-                    if (numberOfCards != 52)
-                        error = new Error('There should be 52 cards, but there are ' + numberOfCards);
+            it('No Card Ranks in DB', done => {
+                CardRankModel.countDocuments({}).then(count => {
+                    if (count)
+                        done(new Error('There are ' + count + ' Card Ranks in the Database'));
+                    else
+                        done();
                 });
-            })
-            .catch(done)
-            .finally(() => {
-                if (error)
-                    done(error);
-                else
-                    done();
             });
+
+            it('No Suits in DB', done => {
+                SuitModel.countDocuments({}).then(count => {
+                    if (count)
+                        done(new Error('There are ' + count + ' Suits in the Database'));
+                    else
+                        done();
+                });
+            });
+
+            it('No Cards in DB', done => {
+                CardModel.countDocuments({}).then(count => {
+                    if (count)
+                        done(new Error('There are ' + count + ' Cards in the Database'));
+                    else
+                        done();
+                });
+            });
+
+        });
+
+        describe('Initializing Cards', () => {
+
+            it('initializeCards()', done => {
+                let error;
+
+                init.initCards().then(() => {
+                    CardModel.countDocuments({}, numberOfCards => {
+                        if (numberOfCards != 52)
+                            error = new Error('There should be 52 cards, but there are ' + numberOfCards);
+                    });
+                })
+                .catch(done)
+                .finally(() => {
+                    if (error)
+                        done(error);
+                    else
+                        done();
+                });
+            });
+
+            it('Check that calling initalize twice doesn\'t create duplicate instances', done => {
+                let error;
+
+                init.initCards().then(() => {
+                    CardModel.countDocuments({}, numberOfCards => {
+                        if (numberOfCards != 52)
+                            error = new Error('There should be 52 cards, but there are ' + numberOfCards);
+                    });
+                })
+                .catch(done)
+                .finally(() => {
+                    if (error)
+                        done(error);
+                    else
+                        done();
+                });
+            });
+
         });
 
     });
 
-})
+    describe('initPoliticalRanks()', () => {
+        
+        before((done) => {
+            PoliticalRankModel.deleteMany({}).then(() => {done()});
+            });
+
+        describe('Check that db collections are empty.', () => {
+
+            it('No Political Ranks in DB', done => {
+                PoliticalRankModel.countDocuments({}).then(count => {
+                    if (count)
+                        done(new Error('There are ' + count + ' Political Ranks in the Database'));
+                    else
+                        done();
+                });
+            });
+
+
+
+        });
+
+        describe('Initializing Political Ranks', () => {
+
+            it('initializePoliticalRanks()', done => {
+                let error;
+
+                init.initPoliticalRanks().then(() => {
+                    PoliticalRankModel.countDocuments({}, numberOfPoliticalRanks => {
+                        if (numberOfPoliticalRanks != 8)
+                            error = new Error('There should be 8 political ranks, but there are ' + numberOfPoliticalRanks);
+                    });
+                })
+                .finally(() => {
+                    if (error)
+                        done(error);
+                    else
+                        done();
+                });
+            });
+
+            it('Check that calling initalize twice doesn\'t create duplicate instances', done => {
+                let error;
+                let testFailed = true;
+
+                init.initPoliticalRanks().then(() => {
+                    PoliticalRankModel.countDocuments({}).then(numberOfPoliticalRanks => {
+                        if (numberOfPoliticalRanks != 8)
+                            error = new Error('There should be 8 political ranks, but there are ' + numberOfPoliticalRanks);
+                        else
+                            testFailed = false;
+                    }).catch(countError => {error = countError});
+                })
+                .finally(() => {
+                    if (testFailed)
+                        done(error);
+                    else
+                        done();
+                });
+            });
+
+        });
+    });
+});
