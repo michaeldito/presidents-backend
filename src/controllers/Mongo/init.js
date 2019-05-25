e920ccb21f857b6909fe4df97ae0d3a9b8026c89
Michael Dito <mcdito13@gmail.com>
2019-05-25 15:01:18 -0700

    fixed mocha stall, tests now use async await and expect

--- src/controllers/Mongo/init.js
+++ src/controllers/Mongo/init.js
@@ -15,19 +15,6 @@ const {
   nextPlayerIdx 
 } = require('../../utils');
 
-const mongoose = require('mongoose');
-
-require('dotenv').config();
-const options = {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-};
-mongoose.connect(process.env.MONGODB_URI_TEST, options).then(
-  () =>  { console.log("[Database] √") },
-  err => { console.log("[Database] X ", err) }
-);
-
-
 
 // init suits
 const suits = [
@@ -175,96 +162,62 @@ const states = [
 
 
 async function initCards() {
-
   const currentNumberOfCards = await CardModel.countDocuments({});
 
-  if (currentNumberOfCards == 52) {
+  if (currentNumberOfCards === 52) {
     return;
   }
-  else {  
-    let promises = [];
 
-    let suitInstances = suits.map(suit => new SuitModel(suit));
-    for (let suitInstance of suitInstances) {
-      promises.push(suitInstance.save(suitInstance));
-    }
-  
-    let cardRankInstances = cardRanks.map(cardRank => new CardRankModel(cardRank));
-    for (let cardRankInstance of cardRankInstances) {
-      promises.push(cardRankInstance.save(cardRankInstance))
-    }
-  
-    // init cards
-    let cards = [];
-  
-    for (suitInstance of suitInstances) {
-      for (cardRankInstance of cardRankInstances) {
-        let shortHand = cardRankInstance.character + suitInstance.name;
-        cards.push({
-          cardRank: cardRankInstance._id,
-          suit: suitInstance._id,
-          shortHand: shortHand
-        });
-      }
-    }
-  
-    let cardInstances = cards.map(card => new CardModel(card));
-    for (let cardInstance of cardInstances) {
-      promises.push(cardInstance.save());
-    }
+  let suitInstances = suits.map(suit => new SuitModel(suit));
+  let suitPromises = suitInstances.map(instance => instance.save());
 
-    return Promise.all(promises);
+  let cardRankInstances = cardRanks.map(cardRank => new CardRankModel(cardRank));
+  let cardRankPromises = cardRankInstances.map(instance => instance.save());
 
+  // init cards
+  let cards = [];
+
+  for (suitInstance of suitInstances) {
+    for (cardRankInstance of cardRankInstances) {
+      let shortHand = cardRankInstance.character + suitInstance.name[0];
+      cards.push({
+        cardRank: cardRankInstance._id,
+        suit: suitInstance._id,
+        shortHand: shortHand
+      });
+    }
   }
 
+  let cardInstances = cards.map(card => new CardModel(card));
+  let cardPromises = cardInstances.map(instance => instance.save());
 
+  return Promise.all([...suitPromises, ...cardRankPromises, ...cardPromises]);
 }
 
 async function initPoliticalRanks() {
-  let politicalRankInstances = politicalRanks.map(rank => new PoliticalRankModel(rank));
-  let promises = [];
-
   let numberOfPoliticalRanks = await PoliticalRankModel.countDocuments({});
-
-  if (numberOfPoliticalRanks == 8) {
+  if (numberOfPoliticalRanks === 8) {
     return;
   }
-  else {
-  
-    for (let politicalRankInstance of politicalRankInstances) {
-      promises.push(politicalRankInstance.save());
-    }
-  
-    return Promise.all(promises);
-
-  }
+  let instances = politicalRanks.map(rank => new PoliticalRankModel(rank));
+  let promises = instances.map(instance => instance.save());
+  return Promise.all(promises);
 }
 
 async function initGameStates() {
-  let stateInstances = states.map(state => new GameStateModel(state));
-  stateInstances.forEach(async (instance) => {
-    try {
-      await instance.save();
-      console.log("[stateInstances Insert] √");
-    } catch(err) {
-      console.log("[stateInstances Insert] X ", err);
-    }
-  });
+  let instances = states.map(state => new GameStateModel(state));
+  let numberOfGameStates = await GameStateModel.countDocuments({});
+
+  if (numberOfGameStates === 3) {
+    return;
+  }
+
+  let promises = instances.map(instance => instance.save());
+  return Promise.all(promises);
 }
 
+module.exports = { initCards, initPoliticalRanks, initGameStates };
 
-
-module.exports.initCards = initCards;
-module.exports.initPoliticalRanks = initPoliticalRanks;
-module.exports.initGameStates = initGameStates;
-
-// make promises that save each instance, then save all
-
-// Promise.all(promises).then(() => {
-//   console.log('docs all saved')
-// }).catch(error => {
-//   console.log(error);
-// })
 
 // let deck = this.createDeck();
 // let shuffledDeck = this.shuffle(deck);
--- test/databaseInitTests.js
+++ test/databaseInitTests.js
@@ -3,77 +3,60 @@ const CardModel = require('../src/models/CardModel/index');
 const CardRankModel = require('../src/models/CardRankModel/index');
 const SuitModel = require('../src/models/SuitModel/index');
 const PoliticalRankModel = require('../src/models/PoliticalRankModel/index');
+const mongoose = require('mongoose');
+const expect = require('expect');
+require('dotenv').config();
 
 describe('Initiallizing Database from scratch.', () => {
 
+    before(async () => {
+        const options = { useNewUrlParser: true, useCreateIndex: true };
+        await mongoose.connect(process.env.MONGODB_URI_TEST, options);
+    });
+
+    after(async () => {
+        await mongoose.connection.close();
+    });
+
     describe('InitCards()', () => {    
         
-        before((done) => {
-            CardModel.deleteMany({}).then(() => {
-                CardRankModel.deleteMany({}).then(() => {
-                    SuitModel.deleteMany({}).then(() => {done()});
-                });
-            });
+        before(async () => {
+            await CardModel.deleteMany({});
+            await CardRankModel.deleteMany({});
+            await SuitModel.deleteMany({});
         });
 
         describe('Check that db collections are empty.', () => {
 
-            it('No Card Ranks in DB', done => {
-                CardRankModel.countDocuments({}).then(count => {
-                    if (count)
-                        done(new Error('There are ' + count + ' Card Ranks in the Database'));
-                    else
-                        done();
-                });
+            it('No Card Ranks in DB', async () => {
+                const count = await CardRankModel.countDocuments({});
+                expect(count).toBe(0);
             });
 
-            it('No Suits in DB', done => {
-                SuitModel.countDocuments({}).then(count => {
-                    if (count)
-                        done(new Error('There are ' + count + ' Suits in the Database'));
-                    else
-                        done();
-                });
+            it('No Suits in DB', async () => {
+                const count = await SuitModel.countDocuments({});
+                expect(count).toBe(0);
             });
 
-            it('No Cards in DB', done => {
-                CardModel.countDocuments({}).then(count => {
-                    if (count)
-                        done(new Error('There are ' + count + ' Cards in the Database'));
-                    else
-                        done();
-                });
+            it('No Cards in DB', async () => {
+                const count = await CardModel.countDocuments({});
+                expect(count).toBe(0);
             });
 
         });
 
         describe('Initializing Cards', () => {
 
-            it('initializeCards()', done => {
-                init.initCards().finally(() => {
-                    CardModel.countDocuments({}, (error, numberOfCards) => {
-                        if (error)
-                            done(error);
-                        else if (numberOfCards != 52)
-                            done(new Error('There should be 52 cards, but there are ' + numberOfCards));
-                        else 
-                            done();
-                    });
-                });
+            it('initializeCards()', async () => {
+                await init.initCards();
+                const count = await CardModel.countDocuments({});
+                expect(count).toBe(52);
             });
 
-            it('Check that calling initalize twice doesn\'t create duplicate instances', done => {
-                init.initCards().finally(() => {
-                    CardModel.countDocuments({}, (error, numberOfCards) => {
-                        if (error)
-                            done(error);
-                        else if (numberOfCards != 52)
-                            done(new Error('There should be 52 cards, but there are ' + numberOfCards));
-                        else 
-                            done();
-                    });
-                });
-
+            it('Check that calling initalize twice doesn\'t create duplicate instances', async () => {
+                await init.initCards();
+                const count = await CardModel.countDocuments({});
+                expect(count).toBe(52);
             });
 
         });
@@ -82,50 +65,35 @@ describe('Initiallizing Database from scratch.', () => {
 
     describe('initPoliticalRanks()', () => {
         
-        before((done) => {
-            PoliticalRankModel.deleteMany({}).then(() => {done()});
-            });
+        before(async () => {
+            await PoliticalRankModel.deleteMany({});
+        });
 
         describe('Check that db collections are empty.', () => {
 
-            it('No Political Ranks in DB', done => {
-                PoliticalRankModel.countDocuments({}).then(count => {
-                    if (count)
-                        done(new Error('There are ' + count + ' Political Ranks in the Database'));
-                    else
-                        done();
-                });
+            it('No Political Ranks in DB', async () => {
+                const count = await PoliticalRankModel.countDocuments({});
+                expect(count).toBe(0);
             });
-
-
 
         });
 
         describe('Initializing Political Ranks', () => {
 
-            it('initializePoliticalRanks()', done => {
-                init.initPoliticalRanks().finally(() => {
-                    PoliticalRankModel.countDocuments({}, (error, numberOfPoliticalRanks) => {
-                        if (numberOfPoliticalRanks != 8)
-                            done(new Error('There should be 8 political ranks, but there are ' + numberOfPoliticalRanks));
-                        else
-                            done();
-                    });
-                });
+            it('initializePoliticalRanks()', async () => {
+                await init.initPoliticalRanks();
+                const count = await PoliticalRankModel.countDocuments({});
+                expect(count).toBe(8);
             });
 
-            it('Check that calling initalize twice doesn\'t create duplicate instances', done => {
-
-                init.initPoliticalRanks().finally(() => {
-                    PoliticalRankModel.countDocuments({}, (error, numberOfPoliticalRanks) => {
-                        if (numberOfPoliticalRanks != 8)
-                            done(new Error('There should be 8 political ranks, but there are ' + numberOfPoliticalRanks));
-                        else
-                            done();
-                    });
-                });
-
+            it('Check that calling initalize twice doesn\'t create duplicate instances', async () => {
+                await init.initPoliticalRanks();
+                const count = await PoliticalRankModel.countDocuments({});
+                expect(count).toBe(8);
             });
+
         });
+
     });
+
 });
