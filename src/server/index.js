7aa74915467130eeb5ac64229671d2fc7baa5a76
Michael Dito <mcdito13@gmail.com>
2019-05-28 23:52:49 -0700

    mocha and should mess around tests

--- src/server/index.js
+++ src/server/index.js
@@ -1,107 +1,16 @@
 require('../config/config');
 
-const fs = require('fs');
-const path = require('path');
-
 const Koa = require('koa');
-const IO = require('koa-socket-2');
 const http = require('http');
 const router = require('../routes');
 const middleware = require('../middleware');
 
 const app = new Koa();
-const io = new IO();
-const chat = new IO('chat');
 
-io.attach(app);
-chat.attach(app);
-
-app.use(middleware);
-
-// serve the html
-app.use(ctx => {
-  ctx.type = 'text/html'
-  ctx.body = fs.createReadStream(path.join(__dirname, 'index.html'));
-});
-
-// Socket middleware
-io.use(async (ctx, next) => {
-  ctx.teststring = 'test'
-  await next();
-});
-
-
-
-/**
- * Socket handlers for IO
- */
-io.on('connection', ctx => {
-  console.log('[koa-socket] - io - connection event', ctx.socket.id);
-
-  io.broadcast('connections', {
-    numConnections: io.connections.size
-  });
-});
-
-io.on('disconnect', ctx => {
-  console.log('[koa-socket] - io - leave event', ctx.socket.id);
-
-  io.broadcast('connections', {
-    numConnections: io.connections.size
-  });
-});
-
-io.on('data', (ctx, data) => {
-  console.log('[koa-socket] - io - data event', ctx.socket.id);
-  console.log('ctx:', ctx.event, ctx.data, ctx.socket.id)
-  console.log('ctx.teststring:', ctx.teststring)
-
-  ctx.socket.emit('response', {
-    message: 'response from server'
-  });
-});
-
-io.on('ack', (ctx, data) => {
-  console.log('[koa-socket] - io - acknowledgement event', ctx.socket.id);
-  ctx.acknowledge('received');
-});
-
-io.on('numConnections', packet => {
-  console.log(`\nNumber of connections: ${io.connections.size}`);
-});
-
-
-/**
- * Chat handlers
- */
-chat.on('connection', ctx => {
-  console.log('[koa-socket] - chat - connction event', ctx.socket.id);
-});
-
-chat.on('message', ctx => {
-  console.log('[koa-socket] - chat - message event', ctx.socket.id);
-
-  // Broadcasts to everybody, including this connection
-  app.chat.broadcast('message', 'yo connections, lets chat')
-
-  // Broadcasts to all other connections
-  ctx.socket.broadcast.emit('message', 'ok connections:chat:broadcast')
-
-  // Emits to just this socket
-  ctx.socket.emit('message', 'ok connections:chat:emit')
-})
-
-app.on('error', (err, ctx) => {
- console.log('woah!');
-});
-
-app.addListener('logging', (ctx) => {
-  console.log('we can log messages like this');
-});
-
+app.use(middleware)
 app.use(router.routes(), router.allowedMethods());
 
-// const server = http.createServer(app.callback());
-app.listen(process.env.PORT, () => console.log(`[Server] listening on PORT ${process.env.PORT}`));
+const server = http.createServer(app.callback());
+server.listen(process.env.PORT, () => console.log(`[Server] listening on PORT ${process.env.PORT}`));
 
-module.exports = app
+module.exports = server
---
+++ src/server/socket.js
@@ -0,0 +1,88 @@
+const io = new IO();
+const chat = new IO('chat');
+const IO = require('koa-socket-2');
+const fs = require('fs');
+const path = require('path');
+const Koa = require('koa');
+const app = new Koa();
+io.attach(app);
+chat.attach(app);
+// set app middleware
+// serve the html
+app.use(ctx => {
+  ctx.type = 'text/html'
+  ctx.body = fs.createReadStream(path.join(__dirname, 'index.html'));
+});
+
+// Socket middleware
+io.use(async (ctx, next) => {
+  ctx.teststring = 'test'
+  await next();
+});
+
+/**
+ * Socket handlers for IO
+ */
+io.on('connection', ctx => {
+  console.log('[koa-socket] - io - connection event', ctx.socket.id);
+
+  io.broadcast('connections', {
+    numConnections: io.connections.size
+  });
+});
+
+io.on('disconnect', ctx => {
+  console.log('[koa-socket] - io - leave event', ctx.socket.id);
+
+  io.broadcast('connections', {
+    numConnections: io.connections.size
+  });
+});
+
+io.on('data', (ctx, data) => {
+  console.log('[koa-socket] - io - data event', ctx.socket.id);
+  console.log('ctx:', ctx.event, ctx.data, ctx.socket.id)
+  console.log('ctx.teststring:', ctx.teststring)
+
+  ctx.socket.emit('response', {
+    message: 'response from server'
+  });
+});
+
+io.on('ack', (ctx, data) => {
+  console.log('[koa-socket] - io - acknowledgement event', ctx.socket.id);
+  ctx.acknowledge('received');
+});
+
+io.on('numConnections', packet => {
+  console.log(`\nNumber of connections: ${io.connections.size}`);
+});
+
+/**
+ * Chat handlers
+ */
+chat.on('connection', ctx => {
+  console.log('[koa-socket] - chat - connction event', ctx.socket.id);
+});
+
+chat.on('message', ctx => {
+  console.log('[koa-socket] - chat - message event', ctx.socket.id);
+
+  // Broadcasts to everybody, including this connection
+  app.chat.broadcast('message', 'yo connections, lets chat')
+
+  // Broadcasts to all other connections
+  ctx.socket.broadcast.emit('message', 'ok connections:chat:broadcast')
+
+  // Emits to just this socket
+  ctx.socket.emit('message', 'ok connections:chat:emit')
+})
+
+app.on('error', (err, ctx) => {
+ console.log('woah!');
+});
+
+app.addListener('logging', (ctx) => {
+  console.log('we can log messages like this');
+});
+// app use routes allowed
