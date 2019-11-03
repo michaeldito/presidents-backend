a66ca6aa30b4bcb5a5809cc44dd227ab9b1700c7
Michael Dito <mcdito13@gmail.com>
2019-11-03 14:07:45 -0800

    deploy

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,8 @@
 const env = process.env.NODE_ENV || 'dev';
 
-if (env === 'dev') {
-  const config = require('./config.json');
-  const envConfig = config[env];
+const config = require('./config.json');
+const envConfig = config[env];
 
-  Object.keys(envConfig).forEach((key) => {
-    process.env[key] = envConfig[key];
-  });
-}
+Object.keys(envConfig).forEach((key) => {
+  process.env[key] = envConfig[key];
+});
