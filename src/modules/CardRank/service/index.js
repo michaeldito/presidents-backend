a6898c2ef6077aac3ca3b09ae8c78df705dd4f3e
Michael Dito <mcdito13@gmail.com>
2020-03-22 03:42:00 -0700

    delete verifyjwt

--- src/modules/CardRank/service/index.js
+++ src/modules/CardRank/service/index.js
@@ -1,6 +1,5 @@
 const Router = require('koa-router');
 const Authenticate = require('../../../middleware/Authenticate')
-const VerifyJWT = require('../../../middleware/VerifyJWT');
 const { getAll, getOne } = require('../controller');
 
 const router = new Router({ prefix: '/cardRanks' });
