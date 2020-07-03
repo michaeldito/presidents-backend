bc48819d7a6aced07734a10dc44a682fb3882769
Michael Dito <mcdito13@gmail.com>
2020-07-02 18:07:29 -0700

    config security

--- src/modules/GameConfiguration/service/index.js
+++ src/modules/GameConfiguration/service/index.js
@@ -3,7 +3,7 @@ const Authenticate = require('../../../middleware/Authenticate')
 const { getAll, getOne } = require('../controller');
 
 const router = new Router({ prefix: '/gameConfigurations' });
-router.get('/', Authenticate(['Admin']), getAll);
+router.get('/', Authenticate(['Admin', 'Player']), getAll);
 router.get('/:id', Authenticate(['Admin']), getOne);
 
 module.exports = router;
